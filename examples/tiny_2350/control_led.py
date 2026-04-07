aea7fa0fa45931250ca22ba5d07569fc5474a4ff
thirdr <ryan@pimoroni.com>
2026-04-07 09:56:01 +0100

    Tiny 2350: example to control on board LED with IR Remote

---
+++ examples/tiny_2350/control_led.py
@@ -0,0 +1,45 @@
+from pimoroni import RGBLED
+
+from aye_arr.nec import NECRemoteReceiver
+from aye_arr.nec.remotes import PimoroniRemote
+
+# Color constants
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+CYAN = (0, 255, 255)
+MAGENTA = (255, 0, 255)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+# Setup the RGB Led
+led = RGBLED(18, 19, 20)
+
+
+def set_led(color, ms, v):
+
+    led.set_rgb(*color)
+
+
+# Create the remote and setup up what each of our buttons will do.
+remote = PimoroniRemote()
+remote.bind("1/RED", (set_led, RED))
+remote.bind("2/GREEN", (set_led, GREEN))
+remote.bind("3/BLUE", (set_led, BLUE))
+remote.bind("4/CYAN", (set_led, CYAN))
+remote.bind("5/MAGENTA", (set_led, MAGENTA))
+remote.bind("6/YELLOW", (set_led, YELLOW))
+remote.bind("OK/STOP", (set_led, BLACK))
+
+receiver = NECRemoteReceiver(12, 1, 0)
+receiver.bind(remote)
+
+try:
+    receiver.start()
+
+    while True:
+        receiver.decode()
+
+finally:
+    receiver.stop()
