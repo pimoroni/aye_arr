c5fee826450aafbb0907a115193376191322281e
thirdr <ryan@pimoroni.com>
2026-04-07 11:36:20 +0100

    Tiny 2350: added example showing different press types

---
+++ examples/tiny_2350/press_types.py
@@ -0,0 +1,48 @@
+from aye_arr.nec.remotes import PimoroniRemote
+from aye_arr.nec import NECRemoteReceiver
+from aye_arr.logging import LOG_NONE
+import time
+
+IR_PIN = 12
+
+
+def press(ms, v):
+    print("Press!")
+
+
+def short(ms, v):
+    print("Short Press!")
+
+
+def release(ms, v):
+    print("Button Released!")
+
+
+def repeat(ms, v):
+    # calculate how long the current button has been held
+    held = (time.ticks_ms() - v) / 1000
+
+    print(f"Repeating/Held button for {held} seconds")
+
+
+# Create the remote and setup up what each of our buttons will do.
+remote = PimoroniRemote()
+
+remote.bind("5/MAGENTA", on_press=press, on_short=None, on_release=release, on_repeat=repeat)
+
+receiver = NECRemoteReceiver(IR_PIN, 1, 0, logging_level=LOG_NONE)
+receiver.bind(remote)
+
+# Wrap the code in a try block, to catch any exceptions (including KeyboardInterrupt)
+try:
+    print("Press (5) on the keypad to test button press types!")
+    receiver.start()
+
+    # Loop until the effect stops or the "Boot" button is pressed
+    while True:
+        receiver.decode()   # Add exception if this is called but start hasn't been called, or is in a stopped state
+
+# Stop any running effects and turn off the LED strip
+finally:
+    receiver.stop()
+    time.sleep(0.1)
